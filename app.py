7d923c6fb26716f1daac66c75f5795d0a59ffc1c
kojowiafe <wiafejeremiah@gmail.com>
2025-01-07 06:18:34 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
+# import csv
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route('/register', methods=["POST"])
+# @app.route('/registrants')
+# def registrants():
+#     return render_template('registered.html', students=students)
+
+@app.route('/register', methods=['POST'])
 def register():
-    name = request.form.get("name")
-    dorm = request.form.get("dorm")
-
-    if not name or not dorm:
-        return render_template("failure.html")
-        # return 'failure'
-    return render_template("success.html")
+    if not request.form.get('name') or not request.form.get('dorm'):
+        return render_template('failure.html')
+    with open('registered.txt', 'a') as file:
+        # writer = csv.writer(file)
+        # writer.writerow((request.form.get('name') +" from " +request.form.get('dorm')))
+        file.write(f'{request.form.get('name')} from {request.form.get('dorm')}\n')
+        return render_template('success.html')
+    
+@app.route('/registered')
+def registered():
+    with open('registered.txt', 'r') as file:
+        # reader = csv.reader(file)
+        # students = list(reader)
+        students = file.readlines()
+        return render_template('registered.html', students=students)
 
 if __name__ == '__main__':
     app.run(debug=True)
